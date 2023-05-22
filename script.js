0c1a494fcd80df9b4fd85817d5e6979448aef118
binyam <binyampowerhc@gmail.com>
2023-05-22 21:44:02 +0300

    fix the linter errors from the script syntax

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const topLogo = document.querySelector(".logo-image img");
-const topMenu = document.querySelector(".humberger img");
-const menuList = document.querySelector(".desk-nav");
+const topLogo = document.querySelector('.logo-image img');
+const topMenu = document.querySelector('.humberger img');
+const menuList = document.querySelector('.desk-nav');
 
-topMenu.addEventListener("click", function(){
-    topLogo.classList.toggle("active");
-    topMenu.classList.toggle("active");
-    menuList.classList.toggle("active");
+topMenu.addEventListener('click', () => {
+  topLogo.classList.toggle('active');
+  topMenu.classList.toggle('active');
+  menuList.classList.toggle('active');
 });
