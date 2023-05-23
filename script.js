90f232ce0bd5eddad3a9175e0b06ba14b545e259
Binyam Tegene <97245616+Log-benjamin@users.noreply.github.com>
2023-05-23 10:50:15 +0300

    add a function to remove mobile-menu on scroll

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 const topLogo = document.querySelector('.logo-image img');
 const topMenu = document.querySelector('.humberger img');
 const menuList = document.querySelector('.desk-nav');
-const menuListOptions = document.querySelectorAll('.desk-nav li a');
+
 topMenu.addEventListener('click', () => {
   topLogo.classList.toggle('active');
   topMenu.classList.toggle('active');
   menuList.classList.toggle('active');
 });
-
-function closeMobileMenu() {
+ 
+window.onscroll = () => {
   topLogo.classList.remove('active');
   topMenu.classList.remove('active');
-  menuList.classList.remove('active');
+  menuList.classList.remove('active');  
 }
-
-menuListOptions.addEventListener(onclick, () => {
-  closeMobileMenu();
-});
